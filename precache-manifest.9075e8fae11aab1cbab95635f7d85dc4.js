6ec669dc146588d6ebc013ccbfaa325d806a9a5a
ahsandatafini <ahsan@datafini.com>
2020-03-10 12:31:03 +0400

    Updates

---
+++ precache-manifest.9075e8fae11aab1cbab95635f7d85dc4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a5745ed1790fbe3efe74fa22804844e",
+    "url": "/react-cinema/index.html"
+  },
+  {
+    "revision": "e50d3db4a3abd5fc53b2",
+    "url": "/react-cinema/static/css/main.1c4a6ebd.chunk.css"
+  },
+  {
+    "revision": "345eec94f955e2410f81",
+    "url": "/react-cinema/static/js/2.2a3077b6.chunk.js"
+  },
+  {
+    "revision": "e50d3db4a3abd5fc53b2",
+    "url": "/react-cinema/static/js/main.73ebd68a.chunk.js"
+  },
+  {
+    "revision": "b2b120cbc2fe890a5be5",
+    "url": "/react-cinema/static/js/runtime-main.a7c45b19.js"
+  }
+]);
